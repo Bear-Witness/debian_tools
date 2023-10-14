46a396d814e8c4681fdfdc2ff8c6ff269a6b6ca4
Santiago Suarez <santisuarezpungitore@hotmail.com>
2023-10-13 23:12:54 -0300

    first tools to download.

---
+++ apt_install_tools.sh
@@ -0,0 +1,10 @@
+
+
+# install text editor
+sudo apt-get install -y vim
+
+# install C developement tools
+sudo apt-get install -y gcc g++ valgrind
+
+# general automation tools
+sudo apt-get install -y make
