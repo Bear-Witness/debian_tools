caea700f6300a179f715282bcdf02127f5d21a1e
Santiago Suarez <santisuarezpungitore@hotmail.com>
2024-02-17 19:11:01 -0300

    Add jq as package to install

--- cli_tools/install.sh
+++ cli_tools/install.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y curl
 # install tmux
 sudo apt-get install -y tmux
 
+# install jq
+sudo apt-get install -y jq
+
 
 # cats all custom functions and adds it to bashrc.
 # a check for duplicated stuff would be nice
